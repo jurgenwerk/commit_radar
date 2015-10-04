60ffbf16a5d61592be18c6998d7ec602147f26e5
Matic Jurglic <matic@jurglic.si>
2015-10-04 20:25:09 +0200

    working test example for geocoding places and adding markers on map

--- app/components/map-area.js
+++ app/components/map-area.js
@@ -2,9 +2,75 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  places: ['Paris, France',
+        'Xiamen, Fujian ',
+        'Paris, France ',
+        'Italy ',
+        'Ohio ',
+        'Paris, France ',
+        'anaheim california ',
+        'United Kingdom ',
+        'Philadelphia, PA ',
+        'Houston, Texas ',
+        'Brooklyn ',
+        'India ',
+        'Rhode Island ',
+        'Buea, Cameroon ',
+        'Rhode Island ',
+        'Bucharest ',
+        'Eindhoven, The Netherlands ',
+        '珠海 ',
+        'São Vicente - SP ',
+        'Italy ',
+        'United Kingdom ',
+        'San Diego ',
+        'Oxford ',
+        'Montreal, QC, Canada ',
+        'Taipei, Taiwan ',
+        'Germany ',
+        'Italy ',
+        'Mumbai, India. ',
+        'Buea, Cameroon ',
+        'France ',
+        'San Francisco, CA ',
+        'Lille, France ',
+        'Manchester, United Kingdom ',
+        'Luxembourg ',
+        'San Francisco, CA ',
+        'United Kingdom ',
+        'Manchester, United Kingdom ',
+        'Rennes, France ',
+        'Grenoble, France ',
+        'Hong Kong',
+        'Warsaw',
+        'Helsinki, Finland'],
+
+  dataIndex: 0,
+
+  placeMarker: function () {
+    let mapObject = $('#map_area').vectorMap('get', 'mapObject');
+    let geocoder = new google.maps.Geocoder();
+    let index = this.dataIndex++;
+    let place = this.places[index];
+    this.set('place', place)
+    geocoder.geocode({'address': place}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        let coords = results[0].geometry.location;
+        console.log(results[0].geometry.location);
+        mapObject.addMarker(index, {latLng: [coords.H, coords.L]});
+      } else {
+        console.log('geocode fail: ' + status);
+      }
+    });
+  },
+
   drawMap: function() {
     this.$('#map_area').vectorMap({
       map: 'world_mill'
     });
+
+    setInterval(() => {
+      this.placeMarker()
+    }, 2000);
   }.on('didInsertElement')
 });
